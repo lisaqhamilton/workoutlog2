854883017f4ea7cbc506e5b4262404edf6c8a480
Lisa Hamilton <lisaqhamilton@gmail.com>
2017-02-01 09:42:26 -0500

    added comments to files

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
 
+//creating a middleware header//
 app.use(require('./middleware/header'));
 
+//creating port 3000 and creating feedback to developer it is on//
 app.listen(3000, function() {
 	console.log("app is listening on 3000");
 });
 
+//creating link to api and creating feedback it is working//
 app.use('/api/test', function(req, res) {
 	res.send("Hello World");
 });
--- server/middleware/header.js
+++ server/middleware/header.js
@@ -1,3 +1,4 @@
+//creating a header which is linked in app.js file//
 module.exports = function(req,res,next) {
 	res.header('access-control-allow-origin', '*');
 	next();
