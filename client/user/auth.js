9640a24134f69280f41fde711a60eada3d4611db
Lisa Hamilton <lisaqhamilton@gmail.com>
2017-02-02 14:24:17 -0500

    Creating signup functions

---
+++ client/user/auth.js
@@ -0,0 +1,42 @@
+$(function() {
+	$.extend(WorkoutLog, {
+		//signup method
+		signup: function() {
+			//username & password variables
+			var username = $("#su_username").val();
+			var password = $("#su_password").val();
+			//user object
+			var user = {
+				user: {
+					username: username,
+					password: password
+				}
+			};
+}
+			//signup post
+			var signup = $.ajax({
+				type: "POST",
+				url: WorkoutLog.API_BASE + "user",
+				data: JSON.stringify(user),
+				contentType: "application/json"
+			});
+			//signup done/fail
+			signup.done(function(data) {
+				if (data.sessionToken) {
+					WorkoutLog.setAuthHeader(data.sesssionToken);
+				}
+				$("#signup-modal").modal("hide");
+				$("#.disabled").removeClass("disabled");
+				$("#loginout").text("Logout");
+					}).fail(function() {
+				$("#su_error").text("There was an issue with sign up").show();
+				});
+		
+		//login method
+
+		//loginoutmethod
+	});
+
+	//blind events
+	$("#signup").on("click", WorkoutLog.signup);
+});
