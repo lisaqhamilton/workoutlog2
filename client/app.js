6fe4d9cd31b810bcf0c83491c53ce18fa8087f0b
Lisa Hamilton <lisaqhamilton@gmail.com>
2017-02-07 15:20:49 -0500

    update button

--- client/app.js
+++ client/app.js
@@ -38,6 +38,9 @@ $(function(){
 		if (target === "#log") {
 			WorkoutLog.log.setDefinitions();
 		}
+		if (target === "#update-log") {
+			WorkoutLog.log.setDefinitions();
+		}
 		if (target  === "#history") {
 		WorkoutLog.log.setHistory();
 	}
--- client/workouts/log.js
+++ client/workouts/log.js
@@ -25,7 +25,7 @@ $(function() {
 					"<div class='pull-right'>" +
 						"<button id='" + history[i].id + "' class='update'><strong>U</strong></button>" +
 						"<button id='" + history[i].id + "' class='remove'><strong>X</strong></button>" +
-						"</div></li>"
+						"</div></li>";
 				}
 				$("#history-list").children().remove();
 				$("#history-list").append(lis);
@@ -36,7 +36,7 @@ $(function() {
 					result: $("#log-result").val(),
 					def: $("#log-definition option:selected").text()
 				};
-				var postData = {log: itsLog};
+				var postData = { log: itsLog };
 				var logger = $.ajax({
 					type: "POST",
 					url: WorkoutLog.API_BASE + "log",
@@ -44,13 +44,62 @@ $(function() {
 					contentType: "application/json"
 				});
 
-				logger.done(function(data) {
-					WorkoutLog.log.workouts.push(data);
-					$("#log-description").val("");
+			logger.done(function(data) {
+				WorkoutLog.log.workouts.push(data);
+				$("#log-description").val("");
 					$("#log-result").val("");
-					$('a[href="#history"]').tab("show");
+					$("a[href='#history']").tab("show");
 				});
 			},
+
+			getWorkout: function() {
+						var thisLog = {id: $(this).attr("id")};
+						console.log(thisLog);
+						logID = thisLog.id;
+						var updateData = { log: thisLog };
+						var getLog = $.ajax({
+							type: "GET",
+							url: WorkoutLog.API_BASE + "log/" + logID,
+							data: JSON.stringify(updateData),
+							contentType: "application/json"
+						});
+					getLog.done(function(data) {
+						$('a[href="#update-log"]').tab("show");
+						$('#update-result').val(data.result);
+						$('#update-description').val(data.description);
+						$('#update-id').val(data.id)
+					});
+				},
+					updateWorkout: function() {
+						$("#update").text("Update");
+						var updateLog = {
+							id: $('#update-id').val(),
+							desc: $('#update-description').val(),
+								result: $('#update-result').val(),
+								def: $('#update-definition option:selected').text()
+						};
+						for (var i = 0; i < WorkoutLog.log.workouts.length; i++) {
+							if(WorkoutLog.log.workouts[i].id == updateLog.id){
+								WorkoutLog.log.workouts.splice(i, 1);
+							}
+						}
+						WorkoutLog.log.workouts.push(updateLog);
+						var updateLogData = { log: updateLog };
+						var updater = $.ajax({
+							type: "PUT",
+							url: WorkoutLog.API_BASE + "log",
+							data: JSON.stringify(updateLogData),
+							contentType: "application/json"
+						});
+
+						updater.done(function(data) {
+							consol.log(data);
+							$("#update-description").val("");
+							$("#update-result").val("");
+							$('a[href="#history"]').tab("show");
+						});
+					},
+
 			delete: function() {
 				var thisLog = {
 					id: $(this).attr("id")
@@ -71,11 +120,9 @@ $(function() {
 				for (var i = 0; i < WorkoutLog.log.workouts.length; i ++){
 					if (WorkoutLog.log.workouts[i].id == thisLog.id){
 						WorkoutLog.log.workouts.splice(i, 1);
-						console.log("I knew you didn't do that workout!")
-					}
+						}
 				}
 				deleteLog.fail(function(){
-					console.log(data),
 					console.log("We're you trying to delete? Ya, that didn't work");
 				});
 			},
@@ -84,8 +131,7 @@ $(function() {
 					type: "GET",
 					url: WorkoutLog.API_BASE + "log",
 					headers: {
-						"authorization":
-					window.localStorage.getItem("sessionToken")
+						"authorization": 	window.localStorage.getItem("sessionToken")
 					}
 				})
 				.done(function(data) {
@@ -98,11 +144,16 @@ $(function() {
 		}
 	});
 
+		
+
 	//Click the button and create a log entry
 	$("#log-save").on("click", WorkoutLog.log.create);
 	$("#history-list").delegate('.remove', 'click', WorkoutLog.log.delete);
-	
+	$("#log-update").on("click",WorkoutLog.log.updateWorkout);
+	$("#history-list").delegate('.update', 'click', WorkoutLog.log.getWorkout);
+
+
 	if (window.localStorage.getItem("sessionToken")) {
 		WorkoutLog.log.fetchAll();
 	}
-})
+});
