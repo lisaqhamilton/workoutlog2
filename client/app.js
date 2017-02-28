52e3469df0752e1b2efe245d5c804867311571cd
Lisa Hamilton <lisaqhamilton@gmail.com>
2017-02-28 08:37:48 -0500

    ui changes

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 $(function(){
 	var WorkoutLog = (function($, undefined) {
-		var API_BASE = "http://localhost:3000/api/";
+		var API_BASE = "https://yourworkoutdiary.herokuapp.com/";
 		var userDefinitions = [ ];
 
 		var setAuthHeader = function(sessionToken) {
